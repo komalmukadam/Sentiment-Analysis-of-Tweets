829fc365f14a552e8ac1fabf7af5b9ee763a844e
Komal <komalmukadam@gmail.com>
2018-05-24 17:23:18 -0500

    Initial commit for #project

---
+++ src/assign3/TweetWithSentiment.java
@@ -0,0 +1,38 @@
+package assign3;
+
+import java.io.Serializable;
+
+/**
+ * Per line sentiment
+ * 
+ * @author shekhargulati
+ * 
+ */
+public class TweetWithSentiment implements Serializable {
+
+	private String line;
+	private String cssClass;
+
+	public TweetWithSentiment() {
+	}
+
+	public TweetWithSentiment(String line, String cssClass) {
+		super();
+		this.line = line;
+		this.cssClass = cssClass;
+	}
+
+	public String getLine() {
+		return line;
+	}
+
+	public String getCssClass() {
+		return cssClass;
+	}
+
+	@Override
+	public String toString() {
+		return "TweetWithSentiment [line=" + line + ", class=" + cssClass + "]";
+	}
+
+}
